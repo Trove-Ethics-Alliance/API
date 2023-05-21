18b62ca4d0f9dae11384f9ff30bde904fe8304fb
Rafski <page+github@kalinowski.app>
2023-05-21 19:41:52 +0200

    21.5.23

--- Addons/ExpressLogs.js
+++ Addons/ExpressLogs.js
@@ -4,11 +4,12 @@ const log = require('./Logger');
 
 module.exports = async (req, res, next) => {
     try {
+        log.warn('IP', req.rawHeaders[1]);
         // Disable the default 'X-Powered-By' header
         app.disable('x-powered-by');
 
         // Log usage of endpoints.
-        log.log(`[ENDPOINT] '${req.method} ${req.originalUrl}' |  IP: '${req.ip}' | Protocol: '${req.protocol}' | User-Agent: '${req.get('User-Agent')}'`);
+        log.log(`[ENDPOINT] '${req.method} ${req.originalUrl}' |  IP: '${req.rawHeaders[1]}' | User-Agent: '${req.get('User-Agent')}'`);
 
         // Set a custom header.
         res.set('X-Powered-By', 'Kalinowski.app');
