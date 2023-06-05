960c24523867ec1bc98eb2f727f397a5c058ef9a
Rafski <page+github@kalinowski.app>
2023-06-05 12:09:31 +0200

    5.6.23

--- Handler/Routes.js
+++ Handler/Routes.js
@@ -28,11 +28,16 @@ async function loadAPIRoutes(app) {
                     // Assign variable to the API Route file.
                     const apiRoute = require(route_dir_root);
 
+                    // String with the API Route, method and file name.
                     let routePath;
+
+                    // Loop through the API Route files and fill the routePath string for table row.
                     for (let index = 0; index < apiRoute.router.stack.length; index++) {
 
                         // Get the route methods toUpperCase string.
                         const methodObject = apiRoute.router.stack[index].route.methods;
+
+                        // Get the route method name to upper case string.
                         const METHOD = Object.keys(methodObject)[0]?.toUpperCase();
 
                         // Make a routePath string with formatted paths and methods.
