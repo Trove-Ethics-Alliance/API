76cfb081c39394518863573a8eec2c8206b37041
Rafski <page+github@kalinowski.app>
2023-08-15 19:05:23 +0200

    15.8.23

--- pm2-express.config.js
+++ pm2-express.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'Express Server',
     instances: 'max',
-    script: './application.js',
+    script: 'node application.js',
     error_file: 'logs/error.log',
     out_file: 'logs/output.log'
   }]
